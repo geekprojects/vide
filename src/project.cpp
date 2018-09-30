6b917cab7e7f4c5d72efc6a8716a762a031ff74e
Ian Parker <ian@geekprojects.com>
2018-09-30 20:56:02 +0100

    Add Copyright and GPL3 details

--- src/project.cpp
+++ src/project.cpp
@@ -1,3 +1,23 @@
+/*
+ * Vide - Vi IDE
+ * Copyright (C) 2018 Ian Parker <ian@geekprojects.com>
+ *
+ * This file is part of Vide.
+ *
+ * Vide is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vide is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vide.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include "project.h"
 
