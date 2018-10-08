6537f083756424896a1bb835b9b0e5e7219de0c6
Ian Parker <ian@geekprojects.com>
2018-10-08 07:51:27 +0100

    Add more test files

---
+++ testproject/src/broken.cpp
@@ -0,0 +1,10 @@
+
+int broken(int a, int b)
+{
+    if (a = b)
+    {
+        return 1;
+    }
+    return b
+}
+
---
+++ testproject/src/long.cpp
@@ -0,0 +1,67 @@
+
+int longfunction()
+{
+    printf("Hello world 0\n");
+    printf("Hello world 1\n");
+    printf("Hello world 2\n");
+    printf("Hello world 3\n");
+    printf("Hello world 4\n");
+    printf("Hello world 7\n");
+    printf("Hello world 6\n");
+    printf("Hello world 7\n");
+    printf("Hello world 8\n");
+    printf("Hello world 9\n");
+    printf("Hello world 10\n");
+    printf("Hello world 11\n");
+    printf("Hello world 12\n");
+    printf("Hello world 13\n");
+    printf("Hello world 14\n");
+    printf("Hello world 17\n");
+    printf("Hello world 16\n");
+    printf("Hello world 17\n");
+    printf("Hello world 18\n");
+    printf("Hello world 19\n");
+    printf("Hello world 20\n");
+    printf("Hello world 21\n");
+    printf("Hello world 22\n");
+    printf("Hello world 23\n");
+    printf("Hello world 24\n");
+    printf("Hello world 27\n");
+    printf("Hello world 26\n");
+    printf("Hello world 27\n");
+    printf("Hello world 28\n");
+    printf("Hello world 29\n");
+    printf("Hello world 30\n");
+    printf("Hello world 31\n");
+    printf("Hello world 32\n");
+    printf("Hello world 33\n");
+    printf("Hello world 34\n");
+    printf("Hello world 37\n");
+    printf("Hello world 36\n");
+    printf("Hello world 37\n");
+    printf("Hello world 38\n");
+    printf("Hello world 39\n");
+    printf("Hello world 40\n");
+    printf("Hello world 41\n");
+    printf("Hello world 42\n");
+    printf("Hello world 43\n");
+    printf("Hello world 44\n");
+    printf("Hello world 47\n");
+    printf("Hello world 46\n");
+    printf("Hello world 47\n");
+    printf("Hello world 48\n");
+    printf("Hello world 49\n");
+    printf("Hello world 50\n");
+    printf("Hello world 51\n");
+    printf("Hello world 52\n");
+    printf("Hello world 53\n");
+    printf("Hello world 54\n");
+    printf("Hello world 57\n");
+    printf("Hello world 56\n");
+    printf("Hello world 57\n");
+    printf("Hello world 58\n");
+    printf("Hello world 59\n");
+
+    return 0;
+}
+
